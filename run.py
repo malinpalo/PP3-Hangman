6046184feb9749aa9f0faf21d7201554420d49d4
malinpalo <malinpalo.mp@gmail.com>
2023-01-03 17:17:11 +0000

    Import sleep from time

--- run.py
+++ run.py
@@ -8,5 +8,6 @@ import os
 from words import hidden_words
 from graphics import welcome, win, loose, tries_left, god_bye
 from colorama import Fore
+from time import sleep
 
 colorama.init(autoreset=True)
